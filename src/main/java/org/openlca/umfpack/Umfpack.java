b12150afe123581427f715b8e344080a05771454
msrocka <michael.srocka@gmail.com>
2018-04-20 13:26:44 +0200

    java side for storing factorized matrices

--- src/main/java/org/openlca/umfpack/Umfpack.java
+++ src/main/java/org/openlca/umfpack/Umfpack.java
@@ -25,6 +25,33 @@ public class Umfpack {
         return result;
     }
 
+    public static native long factorize(
+        int n,
+        int[] columnPointers,
+        int[] rowIndices,
+        double[] values
+    );
+
+    public static UmfFactorizedMatrix factorize(UmfMatrix m) {
+        long pointer = factorize(
+            m.rowCount, 
+            m.columnPointers,
+            m.rowIndices,
+            m.values);
+        return new UmfFactorizedMatrix(pointer);
+    }
+
+    public static native void dispose(long pointer);
+
+    public static native long solveFactorized(
+        long pointer, double[] demand, double[] result);
+    
+    public static double[] solve(UmfFactorizedMatrix m, double[] demand) {
+        double[] result = new double[demand.length];
+        solveFactorized(m.pointer, demand, result);
+        return result;
+    }
+    
     /**
      * Loads the native library from the given path. Does nothing when the
      * library was already loaded.
