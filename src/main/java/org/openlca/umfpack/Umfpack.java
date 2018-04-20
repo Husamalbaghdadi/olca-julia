0d977c7302809eef4fb8567e6a9196ca8944a13d
msrocka <michael.srocka@gmail.com>
2018-04-20 10:58:11 +0200

    added matrix type and conversion

--- src/main/java/org/openlca/umfpack/Umfpack.java
+++ src/main/java/org/openlca/umfpack/Umfpack.java
@@ -1,6 +1,10 @@
 package org.openlca.umfpack;
 
+import java.io.File;
+
 public class Umfpack {
+
+    private static boolean loaded;
 
     public static native void solve(
         int n,
@@ -10,4 +14,27 @@ public class Umfpack {
         double[] demand,
         double[] result);
 
+    public static double[] solve(UmfMatrix m, double[] demand) {
+        double[] result = new double[demand.length];
+        solve(m.rowCount, 
+            m.columnPointers,
+            m.rowIndices,
+            m.values,
+            demand,
+            result);
+        return result;
+    }
+
+    /**
+     * Loads the native library from the given path. Does nothing when the
+     * library was already loaded.
+     */
+    public static void load(String pathToLib) {
+        if (loaded)
+            return;
+        File f = new File(pathToLib);
+        System.load(f.getAbsolutePath());
+        // TODO: test it
+        loaded = true;
+    }
 }
